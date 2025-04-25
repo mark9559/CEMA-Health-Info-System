cd15880822d3da3f359b7072b8589c268fdc671e
DESKTOP-MQ6RL3J\Mark T <mwangimark57@gmail.com>
2025-04-25 11:48:07 +0300

    Created Models

---
+++ server/models.py
@@ -0,0 +1,28 @@
+from app import db
+
+# Association table for many-to-many relationship
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    full_name = db.Column(db.String(100))
+    
+class Enrollment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
+    program_id = db.Column(db.Integer, db.ForeignKey('program.id'), nullable=False)
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(10))
+
+    enrollments = db.relationship('Enrollment', backref='client', cascade='all, delete-orphan')
+
+class Program(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(200))
+
+    enrollments = db.relationship('Enrollment', backref='program', cascade='all, delete-orphan')
